f74649d9dfe971de409c4b486f52ff8295cf49a6
Mike Rivnak <rivnakm1@gmail.com>
2024-03-06 23:41:30 -0500

    fix build

--- build.zig
+++ build.zig
@@ -23,7 +23,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const clap = b.dependency("clap", .{ .target = target, .optimize = optimize });
-    exe.root_module.addImport(clap.artifact("clap"));
+    exe.root_module.addImport("clap", clap.module("clap"));
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,40 @@
 const std = @import("std");
+const clap = @import("clap");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    // First we specify what parameters our program can take.
+    // We can use `parseParamsComptime` to parse a string into an array of `Param(Help)`
+    const params = comptime clap.parseParamsComptime(
+        \\-h, --help             Display this help and exit.
+        \\-n, --number <usize>   An option parameter, which takes a value.
+        \\-s, --string <str>...  An option parameter which can be specified multiple times.
+        \\<str>...
+        \\
+    );
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // Initialize our diagnostics, which can be used for reporting useful errors.
+    // This is optional. You can also pass `.{}` to `clap.parse` if you don't
+    // care about the extra information `Diagnostics` provides.
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
+        .diagnostic = &diag,
+        .allocator = gpa.allocator(),
+    }) catch |err| {
+        // Report useful error and exit
+        diag.report(std.io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer res.deinit();
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    if (res.args.help != 0)
+        std.debug.print("--help\n", .{});
+    if (res.args.number) |n|
+        std.debug.print("--number = {}\n", .{n});
+    for (res.args.string) |s|
+        std.debug.print("--string = {s}\n", .{s});
+    for (res.positionals) |pos|
+        std.debug.print("{s}\n", .{pos});
 }
